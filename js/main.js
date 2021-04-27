af58857f8119722b037b08a1f7ad52dd939668bb
Andrew Clark <clarkandrew10@gmail.com>
2021-04-26 22:49:46 -0400

    4-26 update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-/* =================================
-------------------------------------
-	Civic - CV Resume
-	Version: 1.0
- ------------------------------------
- ====================================*/
-
-
   // Look for .hamburger
   var hamburger = document.querySelector(".hamburger");
   // On click
@@ -13,4 +5,17 @@
     // Toggle class "is-active"
     hamburger.classList.toggle("is-active");
     // Do something else, like open/close menu
+  });
+  // ===== Scroll to Top ====
+  $(window).scroll(function() {
+      if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+          $('#return-to-top').fadeIn(200);    // Fade in the arrow
+      } else {
+          $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+      }
+  });
+  $('#return-to-top').click(function() {      // When arrow is clicked
+      $('body,html').animate({
+          scrollTop : 0                       // Scroll to top of body
+      }, 500);
   });
