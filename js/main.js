73d68b9577b18b6106bcfb8f894f81fe4221d5aa
Andrew Clark <clarkandrew10@gmail.com>
2021-07-16 16:48:35 -0400

    7-16 Finalize and Publish

--- js/main.js
+++ js/main.js
@@ -1,21 +1,86 @@
-  // Look for .hamburger
-  var hamburger = document.querySelector(".hamburger");
-  // On click
-  hamburger.addEventListener("click", function() {
-    // Toggle class "is-active"
-    hamburger.classList.toggle("is-active");
-    // Do something else, like open/close menu
-  });
-  // ===== Scroll to Top ====
-  $(window).scroll(function() {
-      if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-          $('#return-to-top').fadeIn(200);    // Fade in the arrow
-      } else {
-          $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-      }
-  });
-  $('#return-to-top').click(function() {      // When arrow is clicked
-      $('body,html').animate({
-          scrollTop : 0                       // Scroll to top of body
-      }, 500);
-  });
+
+const swup = new Swup();
+
+// ===== Scroll to Top ====
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200); // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200); // Else fade out the arrow
+    }
+});
+
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
+
+
+function hideNav() {
+    var nav = document.getElementById("nav-content")
+    nav.classList.toggle('collapse')
+}
+
+swup.on('clickLink', function() {
+  window.scrollTo(0, 0);
+    setTimeout(function() {
+        
+    }, 1);
+
+});
+swup.on('samePage', function() {
+    window.scrollTo(0, 0);
+});
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
 import webapp2
 import os
 import jinja2
@@ -7,12 +11,45 @@ the_jinja_environment = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+
 class Main(webapp2.RequestHandler):
     def get(self):
         template = the_jinja_environment.get_template("index.html")
         self.response.headers['Content-Type'] = 'text/html'
         self.response.write(template.render())
-
+class Links(webapp2.RequestHandler):
+    def get(self):
+        template = the_jinja_environment.get_template("links.html")
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(template.render())
+class Home(webapp2.RequestHandler):
+    def get(self):
+        template = the_jinja_environment.get_template("home.html")
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(template.render())
+class Cody(webapp2.RequestHandler):
+    def get(self):
+        template = the_jinja_environment.get_template("../work/cody.html")
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(template.render())
+class About(webapp2.RequestHandler):
+    def get(self):
+        template = the_jinja_environment.get_template("about.html")
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(template.render())
+class NotFoundPageHandler(webapp.RequestHandler):
+    def get(self):
+        template = the_jinja_environment.get_template("404.html")
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(template.render())
+        # self.error(404)
+        # self.response.out.write('<Your 404 error html page>')
+        
 app = webapp2.WSGIApplication([
-    ('/', Main)
+    ('/', Main),
+    ('/home', Home),
+    ('/about', About),
+    ('/links', Links),
+    ('/work/cody', Cody),
+    ('/.*', NotFoundPageHandler)
 ], debug=True)
