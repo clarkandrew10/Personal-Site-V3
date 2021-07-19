1e635cc1d2eaa2dea145d2099dba6b0e9018b047
Andrew Clark <clarkandrew10@gmail.com>
2021-07-19 14:41:25 -0400

    Adjusting Homepage

--- js/main.js
+++ js/main.js
@@ -11,62 +11,71 @@ $(window).scroll(function() {
     }
 });
 
-var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
+import Typed from 'typed.js';
+
+var options = {
+  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+  typeSpeed: 40
 };
 
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
+var typed = new Typed('.element', options);
 
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
+// var TxtRotate = function(el, toRotate, period) {
+//   this.toRotate = toRotate;
+//   this.el = el;
+//   this.loopNum = 0;
+//   this.period = parseInt(period, 10) || 2000;
+//   this.txt = '';
+//   this.tick();
+//   this.isDeleting = false;
+// };
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+// TxtRotate.prototype.tick = function() {
+//   var i = this.loopNum % this.toRotate.length;
+//   var fullTxt = this.toRotate[i];
 
-  var that = this;
-  var delta = 300 - Math.random() * 100;
+//   if (this.isDeleting) {
+//     this.txt = fullTxt.substring(0, this.txt.length - 1);
+//   } else {
+//     this.txt = fullTxt.substring(0, this.txt.length + 1);
+//   }
 
-  if (this.isDeleting) { delta /= 2; }
+//   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
+//   var that = this;
+//   var delta = 300 - Math.random() * 100;
 
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
+//   if (this.isDeleting) { delta /= 2; }
 
-window.onload = function() {
-  var elements = document.getElementsByClassName('txt-rotate');
-  for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-  document.body.appendChild(css);
-};
+//   if (!this.isDeleting && this.txt === fullTxt) {
+//     delta = this.period;
+//     this.isDeleting = true;
+//   } else if (this.isDeleting && this.txt === '') {
+//     this.isDeleting = false;
+//     this.loopNum++;
+//     delta = 500;
+//   }
+
+//   setTimeout(function() {
+//     that.tick();
+//   }, delta);
+// };
+
+// window.onload = function() {
+//   var elements = document.getElementsByClassName('txt-rotate');
+//   for (var i=0; i<elements.length; i++) {
+//     var toRotate = elements[i].getAttribute('data-rotate');
+//     var period = elements[i].getAttribute('data-period');
+//     if (toRotate) {
+//       new TxtRotate(elements[i], JSON.parse(toRotate), period);
+//     }
+//   }
+//   // INJECT CSS
+//   var css = document.createElement("style");
+//   css.type = "text/css";
+//   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+//   document.body.appendChild(css);
+// };
 
 
 function hideNav() {
