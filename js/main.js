585952054f7a5c132af57b07b7c47b5183e8d1d1
clarkandrew10 <51393498+clarkandrew10@users.noreply.github.com>
2021-07-21 09:19:03 -0400

    Fixing Typed.js + Swup

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 const swup = new Swup();
 
 // ===== Scroll to Top ====
@@ -11,16 +10,37 @@ $(window).scroll(function() {
     }
 });
 
-console.log("Hello World")
-
 
 swup.on('clickLink', function() {
-  window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
     setTimeout(function() {
-        
+
     }, 1);
 
 });
 swup.on('samePage', function() {
     window.scrollTo(0, 0);
+});
+
+function init() {
+    if (document.querySelector('#typed') != null) {
+        var typed = new Typed('#typed', {
+            strings: ['Hi, I\'m <br>Andrew^2000','Hi, I\'m glad <br>your he','Hi, I\'m glad <br>you\'re here <br>:)','Hi, I\'m a <br>coffee lover ^1000 ☕️','Hi, I\'m a <br>home chef ^1000 🥘'],
+            typeSpeed: 100,
+            backSpeed: 150,
+            smartBackspace: true,
+            loop: true
+        });
+    }
+
+}
+init();
+document.addEventListener('swup:contentReplaced', init);
+
+
+
+document.addEventListener('swup:contentReplaced', event => {
+    swup.options.elements.forEach((selector) => {
+        loadComponents(document.querySelector(selector)); // loads all components inside of replaced block
+    })
 });
