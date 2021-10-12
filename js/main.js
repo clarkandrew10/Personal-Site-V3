2e7517ec8de8157398cc989065e2cc300f199234
clarkandrew10 <51393498+clarkandrew10@users.noreply.github.com>
2021-10-12 16:43:49 -0400

    Fixed the fucking Typed.js + Swup.js error

--- js/main.js
+++ js/main.js
@@ -2,45 +2,47 @@ const swup = new Swup();
 
 // ===== Scroll to Top ====
 
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200); // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200); // Else fade out the arrow
-    }
+$(window).scroll(function () {
+	if ($(this).scrollTop() >= 50) {
+		// If page is scrolled more than 50px
+		$("#return-to-top").fadeIn(200); // Fade in the arrow
+	} else {
+		$("#return-to-top").fadeOut(200); // Else fade out the arrow
+	}
 });
 
-
-swup.on('clickLink', function() {
-    window.scrollTo(0, 0);
-    setTimeout(function() {
-
-    }, 1);
-
+swup.on("clickLink", function () {
+	window.scrollTo(0, 0);
+	setTimeout(function () {}, 1);
 });
-swup.on('samePage', function() {
-    window.scrollTo(0, 0);
+swup.on("samePage", function () {
+	window.scrollTo(0, 0);
 });
 
 function init() {
-    if (document.querySelector('#typed') != null) {
-        var typed = new Typed('#typed', {
-            strings: ['Hey, I\'m Andrew^2000'],
-            typeSpeed: 100,
-            backSpeed: 150,
-            smartBackspace: true,
-            loop: true
-        });
-    }
-
+	if (document.querySelector("#typed") != null) {
+		try {
+			var typed = new Typed("#typed", {
+				strings: ["Hey, I'm Andrew^3000"],
+				typeSpeed: 100,
+				backSpeed: 150,
+				smartBackspace: true,
+				loop: true,
+			});
+		} catch (err) {
+			document.getElementById("typed").innerHTML = "Hey, I'm Andrew";
+		}
+	}
 }
 init();
-document.addEventListener('swup:contentReplaced', init);
+document.addEventListener("swup:contentReplaced", init);
 
-
-
-document.addEventListener('swup:contentReplaced', event => {
-    swup.options.elements.forEach((selector) => {
-        loadComponents(document.querySelector(selector)); // loads all components inside of replaced block
-    })
+document.addEventListener("swup:contentReplaced", (event) => {
+	try {
+		swup.options.elements.forEach((selector) => {
+			loadComponents(document.querySelector(selector)); // loads all components inside of replaced block
+		});
+	} catch (err) {
+		console.log("oops something broke...");
+	}
 });
